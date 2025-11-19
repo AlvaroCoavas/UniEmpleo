c1f1fd544bfe10b2ccb00a5df177cfb888003fbe
AlvaroCoavas <coavascantero@gmail.com>
2025-11-18 23:55:00 -0500

    correccion de errores

--- UniEmpleo/test_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$c = mysqli_connect('localhost','root','','uniempleo');
-if (!$c) { echo 'ERROR:' . mysqli_connect_error(); exit(1); }
-echo 'OK';
